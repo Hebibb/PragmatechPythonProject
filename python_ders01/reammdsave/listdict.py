0f099f43ea0c1da8d71e8fa4ecb3060166cdcf96
Hebibb <bhabibb1995@gmail.com>
2022-03-01 00:00:36 +0400

    simpler bankomat sample

--- python_ders01/reammdsave/listdict.py
+++ python_ders01/reammdsave/listdict.py
@@ -1,5 +1,5 @@
 # Funksiya yazin parameter olaraq list,ve dict qebul etsin. Funksiya yoxlamalidi listin icindeki reqemler dictioneryde yoxdusa onlari silmelidi ve sonda listi return etmelidi. (mes : [27,22,34,44],{"tural": 27,"soltan": 22} funksiyaya gonderirsen o sene [27,22] qaytarir.
-a={"tural": 27,"soltan": 22}
+a={"tural": 27,"soltan": 22,'samir': 44}
 b=[27,22,34,44]
 def hesabla(listim,dictionary):
   topla=0
@@ -11,4 +11,4 @@ def hesabla(listim,dictionary):
   else: return 'Melumati duzgun daxil edin'
   return topla
 
-print(hesabla([12,35,99,103], {'soltan':99,'Qulam': 43,'Isfendiyar':12,'Kerbelayi':65}))
+print(hesabla([12,35,99,103], {'soltan':99,'Qulam': 43,'Isfendiyar':12,'Kerbelayi':65,'sultan':103}))
--- python_ders01/test.py
+++ python_ders01/test.py
@@ -1,2 +1,29 @@
-a='python'
-print(a[-2])
+val_coxalan=[10,20,50,100,200,500]
+val_azalan=val_coxalan[::-1]
+gel=[]
+
+sifir=0
+
+want_money=int(input())
+qalan=want_money-i
+for t in range(10):
+    for i in val_azalan:
+        want_money=qalan
+        if want_money>0:
+            gel.append(i)
+            for j in val_azalan:
+                want_money=qalan
+                if want_money>0:
+                    gel.append(j)
+              
+                else: break
+        else: break
+          
+        
+        
+               
+
+    
+        
+        
+
