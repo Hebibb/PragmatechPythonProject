e368136acc376c3900751a962be865e1bfd7e552
Hebibb <bhabibb1995@gmail.com>
2022-03-01 00:05:19 +0400

    ..

--- python_ders01/bankomat.py
+++ python_ders01/bankomat.py
@@ -1,4 +1,4 @@
-want_amount=int(input())
+want_amount=int(input("Nagd cixaris: "))
 besyuzlukler=int(want_amount/500)
 ikiyuzluk=int((want_amount-besyuzlukler*500)/200)
 yuzluk=int((want_amount-besyuzlukler*500-ikiyuzluk*200)/100)
@@ -8,6 +8,7 @@ onluq=int((want_amount-besyuzlukler*500-ikiyuzluk*200-yuzluk*100-ellilik*50-iyir
 
 if want_amount%10==0:
     print(onluq+iyirmilik+ellilik+yuzluk+ikiyuzluk+besyuzlukler)
+else: print('uzr isteyirik bankomatda 1 veya 5 manatliq eskinaslar yoxdur')
 
 
 
